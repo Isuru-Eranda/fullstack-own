2d66964ce317b9ecbceb1bf84b16a9b3e99e9ac6
Yehan Raveesha <yrlwijesekara@students.nsbm.ac.lk>
2026-01-09 12:29:22 +0530

    Enhance Concession Management page layout and add Add Snacks page

---
+++ frontend/src/pages/admin/addsnacks.jsx
@@ -0,0 +1,8 @@
+export default function AddSnacks() {
+    return (
+        <div>
+            <h1>Add Snacks Page</h1>
+            {/* Add snacks form and content goes here */}
+        </div>
+    );
+}
--- frontend/src/pages/admin/concessionmanagement.jsx
+++ frontend/src/pages/admin/concessionmanagement.jsx
@@ -1,7 +1,10 @@
 export default function ConcessionManagement() {
     return (
-        <div>
-            <h1>Concession Management Page</h1>
+       <div className="min-h-screen bg-background-900 text-text-primary">
+            <button className="fixed right-[60px] bottom-[60px] bg-primary-500 hover:bg-primary-600 text-white font-bold p-4 rounded-full shadow-lg ">
+                Add New Snack
+            </button>
+            <h1 className="text-2xl font-bold m-4">Concession Management Page</h1>
             {/* Concession management content goes here */}
         </div>
     );
